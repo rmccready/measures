e7d96c0391f5a575df6c40840065727629eb0dbd
saulkravitz <skravitz@mitre.org>
2011-01-26 17:03:21 -0500

    Fixed both the c32 test case and the importer spec.  I had previously put the codes into the spec file, and should have put times (in s since the epoch).

--- spec/importer/importer_spec.rb
+++ spec/importer/importer_spec.rb
@@ -22,20 +22,21 @@ describe QME::Importer::GenericImporter do
 
    it "should information from extended NIST example C32 relevant to 0028" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0028', @loader)
-    measure_info['tobacco_non_user'].should include(228512004)
-    measure_info['cessation_counseling'].should include(99407)
+    measure_info['cessation_counseling'].should include(1286668800)
+    measure_info['tobacco_non_user'].should include(-725846400)
+
   end
 
   it "should information from extended NIST example C32 relevant to 0033" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0033', @loader)
-    measure_info['iud_use'].should include(169477005)
-    measure_info['contraceptive_use_education'].should include(99407)
+    measure_info['iud_use'].should include(853459200)
+    measure_info['contraceptive_use_education'].should include(1286668800)
   end
 
 #	0055/N_c47 Diabetes active
  it "should information from extended NIST example C32 relevant to 0055" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0055', @loader)
-    measure_info['diagnosis_diabetes'].should include(111552007)
+    measure_info['diagnosis_diabetes'].should include(-631152000 )
   end
 
 # 0068/N_c191 (acute MI) Active Cardiac Pacemaker in Situ 
@@ -46,32 +47,32 @@ describe QME::Importer::GenericImporter do
 # •	0070/A_c303 Pacemaker 
  it "should information from extended NIST example C32 relevant to 0070" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0070', @loader)
-    measure_info['cardiac_pacer'].should include(14106009)
+    measure_info['cardiac_pacer'].should include(879206400)
   end
 
 # 0081/A_c532 Active Pregnancy  (changed the NIST example, which had a past pregnancy)
 # 0081/A_c39, added 3 records, one each for an allergy, intolerance, and an adverse event (A_518, A_68, A_75)
  it "should information from extended NIST example C32 relevant to 0081" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0081', @loader)
-    measure_info['active_pregnancy'].should include(72892002)
-   measure_info['medication_allergy'].should include(104376)
-    measure_info['medication_intolerance'].should include(104376)
-    measure_info['medication_adverse_event'].should include(104376)
+    measure_info['active_pregnancy'].should include(1291852800)
+   measure_info['medication_allergy'].should include(1134345600)
+    measure_info['medication_intolerance'].should include(1134345600)
+    measure_info['medication_adverse_event'].should include(1134345600)
   end
 # 0389/A_c271 Bone Scan
  it "should information from extended NIST example C32 relevant to 0389" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0389', @loader)
-    measure_info['bone_scan'].should include(169140007)
+    measure_info['bone_scan'].should include(955026000)
   end
 # 0387/A_c278History of Breast Cancer (basically inactive breast cancer) 
  it "should information from extended NIST example C32 relevant to 0387" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0387', @loader)
-    measure_info['breast_cancer_history'].should include(429087003)
+    measure_info['breast_cancer_history'].should include(-631152000 )
   end
 # 0421/Q_C155 BMI Management Plan
  it "should information from extended NIST example C32 relevant to 0421" do
     measure_info = get_measure_info('fixtures/c32_fragments/testingc32.xml', '0421', @loader)
-    measure_info['bmi_management'].should include(225171007)
+    measure_info['bmi_management'].should include(956275200)
   end
 # 	END *** Test cases for extended NIST example C32 file
   it "should import the information relevant to determining hypertension blood pressure measurement" do
