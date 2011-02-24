74261e2f2fe55a0d7b5ba10f020a12d8b18494fb
Marc Hadley <mhadley@mitre.org>
2011-02-24 17:57:44 -0500

    Rework measure 0047 asthma pharma to use generated properties

--- measures/0047_asthma_pharma/0047_NQF_Asthma_Pharmacologic_Therapy.js
+++ measures/0047_asthma_pharma/0047_NQF_Asthma_Pharmacologic_Therapy.js
@@ -14,24 +14,24 @@ function () {
   
   var population = function() {
     correct_age = inRange(patient.birthdate, earliest_birthdate, latest_birthdate);
-    encounters = inRange(measure.encounter, earliest_encounter, effective_date);
-    asthma = inRange(measure.asthma, earliest_encounter, effective_date);
-    persistent_asthma = inRange(measure.persistent_asthma, earliest_encounter, effective_date);
+    encounters = inRange(measure.encounter_office_outpatient_consult_encounter, earliest_encounter, effective_date);
+    asthma = inRange(measure.asthma_diagnosis_active, earliest_encounter, effective_date);
+    persistent_asthma = inRange(measure.asthma_persistent_diagnosis_active, earliest_encounter, effective_date);
     return correct_age && (asthma || persistent_asthma) && encounters>=2;
   }
   
   var denominator = function() {
-    persistent_asthma = inRange(measure.persistent_asthma, earliest_encounter, effective_date);
+    persistent_asthma = inRange(measure.asthma_persistent_diagnosis_active, earliest_encounter, effective_date);
     return persistent_asthma;
   }
   
   var numerator = function() {
-    medication = inRange(measure.asthma_medication, earliest_encounter, effective_date);
+    medication = inRange(measure.corticosteroid_inhaled_or_alternative_asthma_medication_medication_active, earliest_encounter, effective_date);
     return medication;
   }
   
   var exclusion = function() {
-    return inRange(measure.patient_negation_rationale, earliest_encounter, effective_date);
+    return inRange(measure.patient_reason_medication_not_done, earliest_encounter, effective_date);
   }
   
   map(patient, population, denominator, numerator, exclusion);
