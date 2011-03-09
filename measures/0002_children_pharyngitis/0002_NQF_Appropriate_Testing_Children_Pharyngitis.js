87c8cd954391701df7cad2f3459a1c769e4fdac2
saulkravitz <skravitz@mitre.org>
2011-03-09 13:10:16 -0500

    Fixed measure logic so that numerator uses medication events, not encounter events. Also, made sure that measure was ReadOnly.

--- measures/0002_children_pharyngitis/0002_NQF_Appropriate_Testing_Children_Pharyngitis.js
+++ measures/0002_children_pharyngitis/0002_NQF_Appropriate_Testing_Children_Pharyngitis.js
@@ -13,52 +13,72 @@ function () {
   var earliest_birthdate =  effective_date - 18 * year;
   var latest_birthdate =    effective_date - 2 * year;
 
+  var meds_prescribed_after_encounter;  // computed by denominator, used by numerator
+
   var population = function() {
     return inRange(patient.birthdate, earliest_birthdate, latest_birthdate);
   }
   
   var denominator = function() {
+
     var encounters = measure.encounter_ambulatory_including_pediatrics_encounter
     if (!inRange(encounters, earliest_encounter, effective_date))
       return false;
-    if (!inRange(measure.pharyngitis_diagnosis_active, earliest_encounter, effective_date))
+
+    if(!diagnosisDuringEncounter(measure.pharyngitis_diagnosis_active, encounters, earliest_encounter, effective_date) ){
       return false;
+    }
       
     if (!_.isArray(encounters))
       encounters = [encounters];
-    var meds = _.flatten(_.toArray(
-      measure.pharyngitis_antibiotics_medication_dispensed, 
-      measure.pharyngitis_antibiotics_medication_order, 
-      measure.pharyngitis_antibiotics_medication_active));
-    if (!_.isArray(meds))
-      meds = [meds];
-    
+    var meds = _.flatten(_.compact( [ measure.pharyngitis_antibiotics_medication_dispensed, 
+                                      measure.pharyngitis_antibiotics_medication_order, 
+                                      measure.pharyngitis_antibiotics_medication_active ]));
     var result = 0;
     var threeDays = 3 * day;
     var thirtyDays = 30 * day;
     
+    var meds_matched = new Array(meds.length);
+
     var medsThreeAfterAndNotThirtyBefore = function(timeStamp) {
       var match=false;
-      for (i=0; i<meds.length;i++) {
-        if (meds[i]>=timeStamp && (meds[i] <= (timeStamp+threeDays))) {
-          match=true;
-          
-        } else if (meds[i]<=timeStamp && (meds[i] >= (timeStamp-thirtyDays))) {
-          match=false;
-          break;
+      for (var i=0; i<meds.length;i++) {
+        if (meds[i]>=timeStamp){
+            if (meds[i] <= (timeStamp+threeDays)) { // meds within three days of timestamp
+                match=true;  // keep on looking for prior meds
+                // if meds[i] is ever matched as true, mark it as a match for use in the numerator
+                meds_matched[i] = true;
+            }
+        } else if(meds[i] >= (timeStamp-thirtyDays)) { // meds are before timestamp, are they within 30 days prior to timestamp?
+                match=false;
+                break;    // if you find one of these, the search is over
         }
       }
       return match;
-    }
+    };
+
     
     var matchingEncounters = _.select(encounters, medsThreeAfterAndNotThirtyBefore);
-    measure.encounters_when_prescribed_pharyngitis_antibiotics = matchingEncounters;
+
+    meds_prescribed_after_encounter = new Array();
+
+    // The numerator computation uses the medication events that were within 3 days of an appropriate encounter
+    // We've flagged them above, now build an array of them
+    for(var i = 0; i < meds_matched.length; i++){
+	if(meds_matched[i]){
+	    meds_prescribed_after_encounter.push(meds[i]);
+        }
+    }
+       
     return matchingEncounters.length > 0;
-  }
+  };
 
   var numerator = function() {
-    return actionFollowingSomething(measure.encounters_when_prescribed_pharyngitis_antibiotics,
-      measure.group_a_streptococcus_test_laboratory_test_performed, 3*day);
+/* “Laboratory test performed: group A streptococcus test” using “group A streptococcus 	
+test code list grouping” before or simultaneously to <medications> */
+
+    return (actionFollowingSomething(meds_prescribed_after_encounter,
+      measure.group_a_streptococcus_test_laboratory_test_performed, 3*day));
   }
 
   var exclusion = function() {
