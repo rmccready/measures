7c28195c8bf9960576e02479a01fc378895ae278
saulkravitz <skravitz@mitre.org>
2011-03-10 17:13:55 -0500

    Added various 'var' declarations on local variables, and changed logic to eliminate artificial gap of 1 day between pre-natal encounters.

--- measures/0012_hiv_screening/0012_NQF_Prenatal_HIV_Screening.js
+++ measures/0012_hiv_screening/0012_NQF_Prenatal_HIV_Screening.js
@@ -12,35 +12,48 @@ function () {
   var earliest_encounter = effective_date - year;
 
   var population = function() {
-    live_birth_diagnosis = inRange(measure.delivery_live_births_diagnosis_diagnosis_active, earliest_encounter, effective_date);
-    live_birth_procedure = inRange(measure.delivery_live_births_procedure_procedure_performed, earliest_encounter, effective_date);
+    var live_birth_diagnosis = inRange(measure.delivery_live_births_diagnosis_diagnosis_active, earliest_encounter, effective_date);
+    var live_birth_procedure = inRange(measure.delivery_live_births_procedure_procedure_performed, earliest_encounter, effective_date);
     return live_birth_diagnosis && live_birth_procedure;
   }
   
   var denominator = function() {
     if (!measure.estimated_date_of_conception_patient_characteristic)
       return false;
-    estimated_conception = _.max(measure.estimated_date_of_conception_patient_characteristic);
+    var estimated_conception = _.max(measure.estimated_date_of_conception_patient_characteristic);
     return inRange(measure.prenatal_visit_encounter, estimated_conception, effective_date);
   }
 
   var numerator = function() {
-    estimated_conception = _.max(measure.estimated_date_of_conception_patient_characteristic);
-    estimated_conception_within_ten_months = actionFollowingSomething(estimated_conception, measure.delivery_live_births_procedure_procedure_performed, 304*day);
-    encounters_in_range = selectWithinRange(measure.prenatal_visit_encounter, estimated_conception, effective_date);
-    first_encounter = _.min(encounters_in_range);
+    var estimated_conception = _.max(measure.estimated_date_of_conception_patient_characteristic);
+    var estimated_conception_within_ten_months = actionFollowingSomething(estimated_conception, measure.delivery_live_births_procedure_procedure_performed, 304*day);
+/*
+    var encounters_in_range = selectWithinRange(measure.prenatal_visit_encounter, estimated_conception, effective_date);
+    var first_encounter = _.min(encounters_in_range);
     encounters_in_range = selectWithinRange(measure.prenatal_visit_encounter, first_encounter+day, effective_date);
-    second_encounter = _.min(encounters_in_range);
-    hiv_screen_after_first = actionFollowingSomething(first_encounter, measure.hiv_screening_laboratory_test_performed, 30*day);
+    var second_encounter = _.min(encounters_in_range);
+    var hiv_screen_after_first = actionFollowingSomething(first_encounter, measure.hiv_screening_laboratory_test_performed, 30*day);
+    var hiv_screen_after_second = actionFollowingSomething(second_encounter, measure.hiv_screening_laboratory_test_performed, 30*day);
+*/
+    var encounters_in_range = _.sortBy(selectWithinRange(measure.prenatal_visit_encounter, estimated_conception, effective_date), function(num){ return num; });  // there has to be at least 1 due to denominator
+    var first_encounter = encounters_in_range[0];
+    var hiv_screen_after_first = actionFollowingSomething(first_encounter, measure.hiv_screening_laboratory_test_performed, 30*day);
+    var hiv_screen_after_second = false;
+    if(encounters_in_range.length > 1){
+    var second_encounter = encounters_in_range[1];
     hiv_screen_after_second = actionFollowingSomething(second_encounter, measure.hiv_screening_laboratory_test_performed, 30*day);
+    }
+    return estimated_conception_within_ten_months && (hiv_screen_after_first || hiv_screen_after_second);
+
     
     return estimated_conception_within_ten_months && (hiv_screen_after_first || hiv_screen_after_second);
   }
 
   var exclusion = function() {
-    hiv_prior_to_encounter = actionFollowingSomething(measure.hiv_diagnosis_active, measure.prenatal_visit_encounter, year);
-    medical_reason = inRange(measure.medical_reason_laboratory_test_not_done, earliest_encounter, effective_date);
-    patient_reason = inRange(measure.patient_reason_laboratory_test_not_done, earliest_encounter, effective_date);
+    var hiv_prior_to_encounter = actionFollowingSomething(measure.hiv_diagnosis_active, measure.prenatal_visit_encounter, year) +
+                                 actionFollowingSomething(measure.hiv_diagnosis_inactive, measure.prenatal_visit_encounter, year);
+    var medical_reason = inRange(measure.medical_reason_laboratory_test_not_done, earliest_encounter, effective_date);
+    var patient_reason = inRange(measure.patient_reason_laboratory_test_not_done, earliest_encounter, effective_date);
     return hiv_prior_to_encounter || medical_reason || patient_reason;
   }
 
