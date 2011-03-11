bf465a0016b959383237fbcd95b3f2ec563d6647
saulkravitz <skravitz@mitre.org>
2011-03-11 15:07:11 -0500

    Changed time constraints on many denominator events, and adding handling for pregnancy tests AND results.

--- measures/0033_chlamydia_screen/components/0033_NQF_Chlamydia_Screening_All.js
+++ measures/0033_chlamydia_screen/components/0033_NQF_Chlamydia_Screening_All.js
@@ -18,17 +18,22 @@ function () {
   }
 
   var denominator = function() {
-    outpatient_encounter = inRange(measure.encounter_outpatient_encounter, earliest_encounter, effective_date);
-    indicative_procedure = inRange(measure.procedures_indicative_of_sexually_active_woman_procedure_performed, earliest_encounter, effective_date);
-    pregnancy_test = inRange(measure.pregnancy_test_laboratory_test_performed, earliest_encounter, effective_date);
-    iud = inRange(measure.iud_use_device_applied, earliest_encounter, effective_date);
-    education = inRange(measure.contraceptive_use_education_communication_to_patient, earliest_encounter, effective_date);
-    contraceptives = inRange(measure.contraceptives_medication_active, earliest_encounter, effective_date);
-    pregnancy_encounter = inRange(measure.encounter_pregnancy_encounter, earliest_encounter, effective_date);
-    indicative_labs = inRange(measure.laboratory_tests_indicative_of_sexually_active_women_laboratory_test_performed, earliest_encounter, effective_date);
-    active = inRange(measure.sexually_active_woman_diagnosis_active, earliest_encounter, effective_date);
+    // events bounded by measurement period
+    var indicative_procedure = inRange(measure.procedures_indicative_of_sexually_active_woman_procedure_performed, earliest_encounter, effective_date);
+    var pregnancy_test = inRange(measure.pregnancy_test_laboratory_test_performed, earliest_encounter, effective_date) +
+                         inRange(measure.pregnancy_test_laboratory_test_result, earliest_encounter, effective_date);
+
+     // events prior to end of measurement period (could be before measurement period)
+    var indicative_labs = lessThan(measure.laboratory_tests_indicative_of_sexually_active_women_laboratory_test_performed,  effective_date);
+    var outpatient_encounter = lessThan(measure.encounter_outpatient_encounter, effective_date);
+    var iud = lessThan(measure.iud_use_device_applied, earliest_encounter, effective_date);
+    var education = lessThan(measure.contraceptive_use_education_communication_to_patient, effective_date);
+    var contraceptives = lessThan(measure.contraceptives_medication_active, effective_date);
+    var pregnancy_encounter = lessThan(measure.encounter_pregnancy_encounter, effective_date);
+    var active = lessThan(measure.sexually_active_woman_diagnosis_active, effective_date);
     
-    return outpatient_encounter && (indicative_procedure || pregnancy_test || iud || education || contraceptives || pregnancy_encounter || indicative_labs || active);
+    return (outpatient_encounter && 
+           (indicative_procedure || pregnancy_test || iud || education || contraceptives || pregnancy_encounter || indicative_labs || active));
   }
   
   var numerator = function() {
@@ -36,21 +41,33 @@ function () {
   }
   
   var exclusion = function() {
-    pregnancy_test = inRange(measure.pregnancy_test_laboratory_test_performed, earliest_encounter, effective_date);
+    var pregnancy_test = inRange(measure.pregnancy_test_laboratory_test_performed, earliest_encounter, effective_date) +
+                         inRange(measure.pregnancy_test_laboratory_test_result, earliest_encounter, effective_date);
     if (!pregnancy_test)
       return false;
     
     var inMeasureDateRange = function(reading) {
-      result = inRange(reading, earliest_encounter, effective_date);
+      var result = inRange(reading, earliest_encounter, effective_date);
       return result;
     };
     
     if (!_.isArray(measure.pregnancy_test_laboratory_test_performed))
       measure.pregnancy_test_laboratory_test_performed = [measure.pregnancy_test_laboratory_test_performed];
-    pregnancyTestsInMeasureRange = _.select(measure.pregnancy_test_laboratory_test_performed, inMeasureDateRange);
-    
-    retinoid = actionFollowingSomething(pregnancyTestsInMeasureRange, measure.retinoid_medication_active, 7*day);
-    x_ray = actionFollowingSomething(pregnancyTestsInMeasureRange, measure.x_ray_study_diagnostic_study_performed, 7*day);
+    if (!_.isArray(measure.pregnancy_test_laboratory_test_results))
+      measure.pregnancy_test_laboratory_test_result = [measure.pregnancy_test_laboratory_test_result];
+
+    var pregnancyTestsInMeasureRange = _.select(measure.pregnancy_test_laboratory_test_performed, inMeasureDateRange);
+    var pregnancyTestResultsInMeasureRange = _.select(measure.pregnancy_test_laboratory_test_result, inMeasureDateRange);
+   
+    var retinoid = actionFollowingSomething(pregnancyTestResultsInMeasureRange, measure.retinoid_medication_active, 7*day) +
+                   actionFollowingSomething(pregnancyTestResultsInMeasureRange, measure.retinoid_medication_order, 7*day) +
+                   actionFollowingSomething(pregnancyTestResultsInMeasureRange, measure.retinoid_medication_dispensed, 7*day);
+                   actionFollowingSomething(pregnancyTestsInMeasureRange, measure.retinoid_medication_active, 7*day) +
+                   actionFollowingSomething(pregnancyTestsInMeasureRange, measure.retinoid_medication_order, 7*day) +
+                   actionFollowingSomething(pregnancyTestsInMeasureRange, measure.retinoid_medication_dispensed, 7*day);
+
+    var x_ray = actionFollowingSomething(pregnancyTestsInMeasureRange, measure.x_ray_study_diagnostic_study_performed, 7*day) +
+                actionFollowingSomething(pregnancyTestResultsInMeasureRange, measure.retinoid_medication_dispensed, 7*day);
     return retinoid || x_ray;
   }
 
